0536fd90aa7dcc092156d64249d0ef892e2a815e
mahenmondal <mahenmondal111@gmail.com>
2019-05-27 14:02:05 +0530

    first comment

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,48 @@
+import React,{Fragment} from 'react';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import {logout} from '../../actions/auth';
+
+const Navbar = ({auth :{isAuthenticated,loading},logout}) => {
+  const AuthLinks = (
+    <ul>
+      <li><Link  to="/posts">Posts</Link></li>
+       <li><Link  to="/profiles">Developers</Link></li>
+         <li><Link to="/dashboard"><i className="fas fa-user"/>{ ' '}Dashboard</Link></li>
+    <li><a onClick={logout} href="#!">Logout</a></li>
+    
+  </ul>
+  )
+  const GuestLink = (
+    <ul>
+
+    <li><Link  to="/profiles">Developers</Link></li>
+    <li><Link to="/register">Register</Link></li>
+    <li><Link to="/login">Login</Link></li>
+  </ul>
+  )
+    return (
+        <nav className="navbar bg-dark">
+        <h1>
+          <Link to="/"><i className="fas fa-code"></i> DevConnector</Link>
+        </h1>
+          { !loading && (<Fragment>{isAuthenticated ? AuthLinks : GuestLink}</Fragment>)
+            
+
+          }
+      </nav>
+    );
+};
+Navbar.propTypes = {
+  
+  logout:PropTypes.func.isRequired,
+  auth:PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state)=>({
+  auth:state.auth
+})
+
+
+export default connect(mapStateToProps,{logout})(Navbar);
---
+++ routes/api/auth.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const router = express.Router();
+const auth = require('../../middleware/auth');
+const User = require("../../models/User");
+const {check, validationResult} = require('express-validator/check');
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+router.get("/",auth ,async(req,res)=>{
+    try{
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
+router.post("/",[
+  
+    
+    check('email','please add a valid email').isEmail(),
+    check('password','Please add more than 6 digit').exists()
+  ],async (req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const {email,password} = req.body ;
+      try{
+        let user = await User.findOne({email});
+        if(!user){
+           return res.status(400).json({ errors: [{msg :'invalid credentials'}] });
+        }
+       
+        const isMatch = await bcrypt.compare(password,user.password);
+        
+        if(!isMatch){
+            return res.status(400).json({ errors: [{msg :'invalid credentials'}] });
+        }
+
+        const payload ={
+            user:{
+                id:user.id
+            }
+        }
+        jwt.sign(payload,config.get('jwtSecret'),{expiresIn:360000},(err,token)=> {
+            if(err) throw err ;
+            res.json({token});
+        });
+
+        
+      }catch(err){
+          console.error(err.message);
+          res.status(500).send('Server error');
+      }
+
+    
+})
+
+module.exports = router ;
